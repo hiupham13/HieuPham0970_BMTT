1d9956386904ddaf2a9cdec067c6c3504180b3c4
hiupham13 <hieupham13012004@gmail.com>
2025-04-02 10:10:37 +0700

    lab05-ssl

--- Lab05/ssl/server.py
+++ Lab05/ssl/server.py
@@ -27,4 +27,14 @@ def handle_client(client_socket):
         clients.remove(client_socket)
         client_socket.close()
 
-server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(server_address)
+server_socket.listen(5)
+print ("Server đang chờ kết nối...")
+while True:
+    client_socket,client_address = server_socket.accept()
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    context.load_cert_chain(certfile='./certificates/server-cert.crt', keyfile='./certificates/server-key.key')
+    ssl_socket = context.wrap_socket(client_socket, server_side=True)
+    client_thread = threading.Thread(target=handle_client, args=(ssl_socket,))
+    client_thread.start()
